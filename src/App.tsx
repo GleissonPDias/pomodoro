b167be4b777baa7e104680a7ef6dd8dec29d4bbc
GleissonPDias <gleisson.gpd10@gmail.com>
2025-06-24 01:27:53 -0300

    adicao de mais componentes como input e ciclos

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
+import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -19,6 +21,29 @@ export function App() {
 
       <Container>
         <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+          <div className="formRow">
+            <DefaultInput
+              labelText="Task"
+              id="meuInput"
+              placeholder="Digite uma tarefa:"
+              type="text"
+            />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet</p>
+          </div>
+          <div className="formRow">
+            <Cycles />
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
